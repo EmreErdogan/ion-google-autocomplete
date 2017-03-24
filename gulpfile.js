a7938f7cd59fa32a63c381d4ad6ad4a37d12adae
Emre Erdoğan <mustafa.emre.erdogan@gmail.com>
2017-03-24 16:35:49 +0300

    add gulp-ng-annotate, gulp-jshint, jshint-stylish, gulp-strip-debug and update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 var del = require('del');
 var concat = require("gulp-concat");
+var ngAnnotate = require('gulp-ng-annotate');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var stripDebug = require('gulp-strip-debug');
 
 // clean the contents of the distribution directory
 gulp.task('clean', function () {
@@ -11,6 +15,10 @@ gulp.task('clean', function () {
 gulp.task('compile', ['clean'], function () {
   return gulp
     .src('src/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish))
+    .pipe(ngAnnotate())
+    .pipe(stripDebug())
     .pipe(concat('ion-google-autocomplete.js'))
     .pipe(gulp.dest('dist'));
 });
